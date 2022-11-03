b17cb4d6548891277402043067a88ed95a740324
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2022-11-03 17:47:21 +0100

    WIP

---
+++ JS/js.js
@@ -0,0 +1,6 @@
+ function showMenu(){
+    var element = document.getElementById("ham")
+    var bodyOpacity = document.getElementById("opacity")
+    element.classList.toggle("hidden")
+    bodyOpacity.classList.toggle("opacity-50")
+}
