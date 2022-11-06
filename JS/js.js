c0b5db803eb769d97ca40c917c92a54938182685
Thomas Grossmann <thomas.grossmann@cpnv.ch>
2022-11-06 01:29:41 +0100

    Game Page - Historic Scroll to finish

--- JS/js.js
+++ JS/js.js
@@ -1,25 +1,29 @@
 function showMenu() {
-    var element = document.getElementById("ham")
+    // Getter of HTML elements
+    var ham = document.getElementById("ham")
     var bodyOpacity = document.getElementById("opacity")
     var iconMenu = document.getElementById("iconMenu")
-    element.classList.toggle("hidden")
+
+    // Toggle classes to make menu appears from the top
+    ham.classList.toggle("-top-96")
+    ham.classList.toggle("top-24")
+
+    // Toggle body opacity
     bodyOpacity.classList.toggle("opacity-50")
-    
+
     // Change navigation menu icon when clicked
-    if(iconMenu.classList.contains("fa-bars")) {
+    if (iconMenu.classList.contains("fa-bars")) {
         iconMenu.classList.add("fa-xmark")
         iconMenu.classList.remove("fa-bars")
     } else {
         iconMenu.classList.remove("fa-xmark")
         iconMenu.classList.add("fa-bars")
     }
-    
+
 
     // Hide the navigation menu when click outside of the menu
-    /*window.addEventListener('click', function (e) {
-        if (!document.getElementById('ham').contains(e.target)) {
-            element.classList.toggle("hidden")
-            bodyOpacity.classList.toggle("opacity-50")
-        }
-    });*/
+    if (!document.getElementById('ham').contains(e.target)) {
+        element.classList.toggle("hidden")
+        bodyOpacity.classList.toggle("opacity-50")
+    };
 }
